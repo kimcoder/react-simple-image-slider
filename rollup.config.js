59fe63993d8252a3a165fdd5209a88ba9e261506
kimcoder <kimcoder@me.com>
2020-08-28 19:46:21 +0900

    [feature/2.0.0] rollup typescript setting add

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,26 @@
 import babel from "rollup-plugin-babel";
 import image from "rollup-plugin-img";
+import typescript from 'rollup-plugin-typescript2';
 import pkg from "./package.json";
 
 const external = id => !id.startsWith("/") && !id.startsWith(".");
 
 export default {
-    input: "./src/ImageSlider.jsx",
+    input: "src/index.ts",
     output: {
         file: pkg.main,
-        format: "cjs",
+        format: "esm",
     },
     plugins: [
         babel({ runtimeHelpers: true }),
         image({ limit: 1000 }),
+        typescript({
+        tsconfig: 'tsconfig.json',
+        tsconfigOverride: {
+            include: [ 'src/**/*' ],
+            exclude: [ 'node_modules', 'example' ]
+          }
+        })
     ],
     external,
 };
