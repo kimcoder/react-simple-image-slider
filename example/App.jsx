b705fde24d80d3425d8f64ffe98d141f750ddbff
Łukasz <elektrybaut@gmail.com>
2020-10-11 23:49:25 +0200

    [feature/urlImageAccessor] added a prop to customize images array url field name

--- example/App.jsx
+++ example/App.jsx
@@ -14,153 +14,161 @@ import Select from "@material-ui/core/Select";
 import SimpleImageSlider from "..";
 
 class App extends React.Component {
-    listSubHeader = <ListSubheader><h1>Slider Settings</h1></ListSubheader>;
+  listSubHeader = (
+      <ListSubheader>
+          <h1>Slider Settings</h1>
+      </ListSubheader>
+  );
 
-    toggleOptions = ["useGPURender", "showNavs", "showBullets"];
+  toggleOptions = ["useGPURender", "showNavs", "showBullets"];
 
-    images = [
-        { url: "images/1.jpg" },
-        { url: "images/2.jpg" },
-        { url: "images/3.jpg" },
-        { url: "images/4.jpg" },
-        { url: "images/5.jpg" },
-        { url: "images/6.jpg" },
-        { url: "images/7.jpg" },
-    ];
+  images = [
+      { uri: "images/1.jpg" },
+      { uri: "images/2.jpg" },
+      { uri: "images/3.jpg" },
+      { uri: "images/4.jpg" },
+      { uri: "images/5.jpg" },
+      { uri: "images/6.jpg" },
+      { uri: "images/7.jpg" },
+  ];
 
-    constructor() {
-        super();
-        this.state = {
-            useGPURender: true,
-            showNavs: true,
-            showBullets: true,
-            navStyle: 1,
-            slideDuration: 0.5,
-            bgColor: "#000000",
-            slideIndexText: "",
-        };
-    }
+  constructor() {
+      super();
+      this.state = {
+          useGPURender: true,
+          showNavs: true,
+          showBullets: true,
+          navStyle: 1,
+          slideDuration: 0.5,
+          bgColor: "#000000",
+          slideIndexText: "",
+      };
+  }
 
-    componentDidMount() {
-        console.log("[App componentDidMount]");
-    }
+  componentDidMount() {
+      console.log("[App componentDidMount]");
+  }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("[App componentDidUpdate]");
-    }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+      console.log("[App componentDidUpdate]");
+  }
 
-    onClick = (idx, event) => {
-        console.log(`[App onClick] ${idx} ${event.target}`);
-    }
+  onClick = (idx, event) => {
+      console.log(`[App onClick] ${idx} ${event.target}`);
+  };
 
-    onClickNav = (toRight) => {
-        console.log(`[App onClickNav] ${toRight}`);
-    }
+  onClickNav = (toRight) => {
+      console.log(`[App onClickNav] ${toRight}`);
+  };
 
-    onClickBullets = (idx) => {
-        console.log(`[App onClickBullets] ${idx}`);
-    }
+  onClickBullets = (idx) => {
+      console.log(`[App onClickBullets] ${idx}`);
+  };
 
-    onStartSlide = (idx, length) => {
-        console.log(`[App onStartSlide] ${idx}/${length}`);
-        this.setState({ slideIndexText: `${idx} / ${length}`});
-    }
+  onStartSlide = (idx, length) => {
+      console.log(`[App onStartSlide] ${idx}/${length}`);
+      this.setState({ slideIndexText: `${idx} / ${length}` });
+  };
 
-    onCompleteSlide = (idx, length) => {
-        console.log(`[App onCompleteSlide] ${idx}/${length}`);
-        this.setState({ slideIndexText: `${idx} / ${length}`});
-    }
+  onCompleteSlide = (idx, length) => {
+      console.log(`[App onCompleteSlide] ${idx}/${length}`);
+      this.setState({ slideIndexText: `${idx} / ${length}` });
+  };
 
-    onToggleOptions = value => () => {
-        console.log(`[App onToggleOptions] ${value}`);
-        const updateValue = !this.state[value];
-        this.setState({ [value]: updateValue });
-    }
+  onToggleOptions = value => () => {
+      console.log(`[App onToggleOptions] ${value}`);
+      const updateValue = !this.state[value];
+      this.setState({ [value]: updateValue });
+  };
 
-    onChangeSelect = event => this.setState({ [event.target.name]: event.target.value });
+  onChangeSelect = event => this.setState({ [event.target.name]: event.target.value });
 
-    render() {
-        const slideText = this.state.slideIndexText || `${1} / ${this.images.length}`;
+  render() {
+      const slideText = this.state.slideIndexText || `${1} / ${this.images.length}`;
 
-        return (
-            <div style={{ textAlign: "center" }}>
-                <CssBaseline />
-                <AppBar style={{ position: "relative", height: 140, textAlign: "center" }}>
-                    <h1 style={{ marginBottom: 5 }}>React Simple Image Slider Example</h1>
-                    <p>simple image slider component for react</p>
-                    <div>
-                        <iframe
-                            title="git icon"
-                            src="https://ghbtns.com/github-btn.html?user=kimcoder&repo=react-simple-image-slider&type=star"
-                            frameBorder="0"
-                            width="51px"
-                            height="30px"
-                        />
-                    </div>
-                </AppBar>
-                <SimpleImageSlider
-                    style={{ margin: "0 auto", marginTop: "50px" }}
-                    width={896}
-                    height={504}
-                    images={this.images}
-                    showBullets={this.state.showBullets}
-                    showNavs={this.state.showNavs}
-                    useGPURender={this.state.useGPURender}
-                    navStyle={this.state.navStyle}
-                    slideDuration={this.state.slideDuration}
-                    onClick={this.onClick}
-                    onClickNav={this.onClickNav}
-                    onClickBullets={this.onClickBullets}
-                    onStartSlide={this.onStartSlide}
-                    onCompleteSlide={this.onCompleteSlide}
-                />
+      return (
+          <div style={{ textAlign: "center" }}>
+              <CssBaseline />
+              <AppBar
+                  style={{ position: "relative", height: 140, textAlign: "center" }}
+              >
+                  <h1 style={{ marginBottom: 5 }}>React Simple Image Slider Example</h1>
+                  <p>simple image slider component for react</p>
+                  <div>
+                      <iframe
+                          title="git icon"
+                          src="https://ghbtns.com/github-btn.html?user=kimcoder&repo=react-simple-image-slider&type=star"
+                          frameBorder="0"
+                          width="51px"
+                          height="30px"
+                      />
+                  </div>
+              </AppBar>
+              <SimpleImageSlider
+                  style={{ margin: "0 auto", marginTop: "50px" }}
+                  width={896}
+                  height={504}
+                  images={this.images}
+                  imageUrlAccessor="uri"
+                  showBullets={this.state.showBullets}
+                  showNavs={this.state.showNavs}
+                  useGPURender={this.state.useGPURender}
+                  navStyle={this.state.navStyle}
+                  slideDuration={this.state.slideDuration}
+                  onClick={this.onClick}
+                  onClickNav={this.onClickNav}
+                  onClickBullets={this.onClickBullets}
+                  onStartSlide={this.onStartSlide}
+                  onCompleteSlide={this.onCompleteSlide}
+              />
 
-                <div style={{ margin: "10px" }}>
-                    {slideText}
-                </div>
+              <div style={{ margin: "10px" }}>{slideText}</div>
 
-                {
-                    // Slide Settings
-                }
-                <List subheader={this.listSubHeader} style={{ margin: "0 auto 100px auto", width: 900, textAlign: "left" }}>
-                    {this.toggleOptions.map(value => (
-                        <ListItem key={value} button onClick={this.onToggleOptions(value)}>
-                            <Checkbox checked={this.state[value]} disableRipple />
-                            <ListItemText primary={`${value}`} />
-                        </ListItem>
-                    ))}
-                    <ListItem>
-                        <FormControl component="fieldset">
-                            <InputLabel>NavStyle</InputLabel>
-                            <Select
-                                value={this.state.navStyle}
-                                onChange={this.onChangeSelect}
-                                inputProps={{ name: "navStyle" }}
-                            >
-                                <MenuItem value={1}>1</MenuItem>
-                                <MenuItem value={2}>2</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </ListItem>
-                    <ListItem>
-                        <FormControl>
-                            <InputLabel>slideDuration</InputLabel>
-                            <Select
-                                value={this.state.slideDuration}
-                                onChange={this.onChangeSelect}
-                                inputProps={{ name: "slideDuration" }}
-                            >
-                                <MenuItem value={0.3}>0.3</MenuItem>
-                                <MenuItem value={0.5}>0.5</MenuItem>
-                                <MenuItem value={0.7}>0.9</MenuItem>
-                                <MenuItem value={1.2}>1.2</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </ListItem>
-                </List>
-            </div>
-        );
-    }
+              {
+                  // Slide Settings
+              }
+              <List
+                  subheader={this.listSubHeader}
+                  style={{ margin: "0 auto 100px auto", width: 900, textAlign: "left" }}
+              >
+                  {this.toggleOptions.map(value => (
+                      <ListItem key={value} button onClick={this.onToggleOptions(value)}>
+                          <Checkbox checked={this.state[value]} disableRipple />
+                          <ListItemText primary={`${value}`} />
+                      </ListItem>
+                  ))}
+                  <ListItem>
+                      <FormControl component="fieldset">
+                          <InputLabel>NavStyle</InputLabel>
+                          <Select
+                              value={this.state.navStyle}
+                              onChange={this.onChangeSelect}
+                              inputProps={{ name: "navStyle" }}
+                          >
+                              <MenuItem value={1}>1</MenuItem>
+                              <MenuItem value={2}>2</MenuItem>
+                          </Select>
+                      </FormControl>
+                  </ListItem>
+                  <ListItem>
+                      <FormControl>
+                          <InputLabel>slideDuration</InputLabel>
+                          <Select
+                              value={this.state.slideDuration}
+                              onChange={this.onChangeSelect}
+                              inputProps={{ name: "slideDuration" }}
+                          >
+                              <MenuItem value={0.3}>0.3</MenuItem>
+                              <MenuItem value={0.5}>0.5</MenuItem>
+                              <MenuItem value={0.7}>0.9</MenuItem>
+                              <MenuItem value={1.2}>1.2</MenuItem>
+                          </Select>
+                      </FormControl>
+                  </ListItem>
+              </List>
+          </div>
+      );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("App"));
--- src/ImageSliderPropTypes.js
+++ src/ImageSliderPropTypes.js
@@ -1,21 +1,45 @@
 import PropTypes from "prop-types";
 
-const isValidNavStyle = prop => (/[1-2]/.test(prop) && typeof (prop) === "number");
+const isValidNavStyle = prop => /[1-2]/.test(prop) && typeof prop === "number";
 
 export default {
     propTypes: {
-        // Required
-        width: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-        ]).isRequired,
-        height: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-        ]).isRequired,
-        images: PropTypes.arrayOf(PropTypes.shape({
-            url: PropTypes.string.isRequired,
-        })).isRequired,
+    // Required
+        width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+        height: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+            .isRequired,
+        images: (props, propName, componentName) => {
+            const propValue = props[propName];
+            if (!propValue) {
+                return new Error(
+                    `Warning: Prop type ${propName} is required in ${componentName}.`,
+                );
+            }
+            if (!Array.isArray(propValue)) {
+                return new Error(
+                    `Warning: Failed prop type: Invalid prop ${propName} of type ${typeof propValue} supplied to ${componentName}, expected Array.`,
+                );
+            }
+            let error = null;
+            if (propValue.length > 0) {
+                const urlAccessor = props.imageUrlAccessor;
+                propValue.forEach((val) => {
+                    if (typeof val !== "object" || val === null) {
+                        error = new Error(
+                            `Warning: Invalid prop ${propName} array element of type ${typeof val} supplied to ${componentName}, expected Object`,
+                        );
+                        return;
+                    }
+
+                    if (!Object.prototype.hasOwnProperty.call(val, urlAccessor)) {
+                        error = new Error(
+                            `Missing object url field name for the ${propName} supplied to ${componentName}. Should be "${urlAccessor}".`,
+                        );
+                    }
+                });
+            }
+            return error;
+        },
 
         // Optional
         style: PropTypes.objectOf(PropTypes.string),
@@ -24,6 +48,7 @@ export default {
         showBullets: PropTypes.bool,
         bgColor: PropTypes.string,
         useGPURender: PropTypes.bool,
+        imageUrlAccessor: PropTypes.string.isRequired,
         onClick: PropTypes.func,
         onClickNav: PropTypes.func,
         onClickBullets: PropTypes.func,
@@ -33,7 +58,9 @@ export default {
         // Optional, Navigation Arrow Style
         navStyle: (props, propName, componentName) => {
             if (!isValidNavStyle(props[propName])) {
-                return new Error(`Invalid prop ${propName} supplied to ${componentName}. Validation failed.`);
+                return new Error(
+                    `Invalid prop ${propName} supplied to ${componentName}. Validation failed.`,
+                );
             }
             return null;
         },
@@ -45,5 +72,6 @@ export default {
         bgColor: "#000000",
         useGPURender: true,
         navStyle: 1,
+        imageUrlAccessor: "url",
     },
 };
